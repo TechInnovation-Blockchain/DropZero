cc4ebd1f95131baeaa5ad11579174b6075240c50
TechInnovation-Blockchain <newcaesar628@gmail.com>
2021-03-31 16:13:04 +0500

    Web3 provider issue fixed

--- src/contracts/getContract.js
+++ src/contracts/getContract.js
@@ -1,31 +1,32 @@
-import web3 from 'web3';
+import Web3 from "web3";
 
-import { abi as erc20Abi } from './abi/erc20Abi.json';
-import { logError } from '../utils/log';
+import { abi as erc20Abi } from "./abi/erc20Abi.json";
+import { logError } from "../utils/log";
 
-let Web3;
-let Web3Infura;
+let web3;
+let web3Infura;
 
 try {
-  Web3Infura = new web3.providers.HttpProvider(
+  web3 = new Web3(window?.web3?.currentProvider);
+  web3Infura = new Web3(
     `https://rinkeby.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`
   );
-  Web3 = new web3(window.web3?.currentProvider);
-  console.log(Web3Infura);
+
+  // console.log("WEB", web3Infura);
 } catch (e) {
-  logError('Connect Web3', e);
+  logError("Connect Web3", e);
 }
 
-export const erc20TokenContract = tokenAddress => {
+export const erc20TokenContract = (tokenAddress) => {
   let contract;
   try {
-    if (Web3) {
-      contract = new Web3.eth.Contract(erc20Abi, tokenAddress);
+    if (window?.web3?.currentProvider && web3) {
+      contract = new web3.eth.Contract(erc20Abi, tokenAddress);
     } else {
-      contract = new Web3Infura.eth.Contract(erc20Abi, tokenAddress);
+      contract = new web3Infura.eth.Contract(erc20Abi, tokenAddress);
     }
     return contract;
   } catch (e) {
-    logError('erc20TokenContract', e);
+    logError("erc20TokenContract", e);
   }
 };
