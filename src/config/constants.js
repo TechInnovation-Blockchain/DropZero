1e6cd3c5b90e43e45474e52f9db78925f03ef4f2
TechInnovation-Blockchain <newcaesar628@gmail.com>
2021-04-17 02:54:59 +0500

    contract address updated

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,37 +1,38 @@
 export const CONTRACT_ADDRESSES = {
-  dropFactory: '0xf913b2A5FeC65C237eEE959A34A0e5Ee81955fCC',
+  dropFactory: "0x082046319ad7ddB5993F796FaFE6c0439c8fd5Fe",
 };
 
-export const BASE_URL = 'https://dropzero.herokuapp.com';
+export const BASE_URL = "https://dropzero.herokuapp.com";
 
 //export const BASE_URL = 'http://192.168.1.79:8000';
 
 export const config = {
   headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
   },
 };
 
 export const formDataConfig = {
   headers: {
-    'Content-Type': 'multipart/form-data',
+    "Content-Type": "multipart/form-data",
   },
 };
 
 export const NoLogo =
-  'https://gateway.pinata.cloud/ipfs/QmNX2QerTxTm1RThD7Dc9X5uS9VFnQxmMotaMFhK5GYbBk';
+  "https://gateway.pinata.cloud/ipfs/QmNX2QerTxTm1RThD7Dc9X5uS9VFnQxmMotaMFhK5GYbBk";
 
-export const DATE_FORMAT = 'dd MMM yyyy';
+export const DATE_FORMAT = "dd MMM yyyy";
 
 export const ETHERSCAN_TX_BASE_URL =
-  process.env.REACT_APP_ENVIRONMENT === 'PRODUCTION'
-    ? 'https://etherscan.io/tx/'
-    : 'https://rinkeby.etherscan.io/tx/';
+  process.env.REACT_APP_ENVIRONMENT === "PRODUCTION"
+    ? "https://etherscan.io/tx/"
+    : "https://rinkeby.etherscan.io/tx/";
 
 export const ETHERSCAN_ADDRESS_BASE_URL =
-  process.env.REACT_APP_ENVIRONMENT === 'PRODUCTION'
-    ? 'https://etherscan.io/address/'
-    : 'https://rinkeby.etherscan.io/address/';
+  process.env.REACT_APP_ENVIRONMENT === "PRODUCTION"
+    ? "https://etherscan.io/address/"
+    : "https://rinkeby.etherscan.io/address/";
 
-export const VALID_CHAIN = process.env.REACT_APP_ENVIRONMENT === 'PRODUCTION' ? 1 : 4;
+export const VALID_CHAIN =
+  process.env.REACT_APP_ENVIRONMENT === "PRODUCTION" ? 1 : 4;
